f2d65504c5235b689cfa0b1b12228c08263012e2
Abdelaziz ELMOUSSAOUI <abdelaziz.elmoussaoui@tessi.fr>
2026-05-09 23:13:05 +0100

    Problem access with Ip adress resolve

--- hrms/src/main/java/com/example/hrms/security/SecurityConfig.java
+++ hrms/src/main/java/com/example/hrms/security/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
